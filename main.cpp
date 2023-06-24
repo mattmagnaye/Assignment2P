20a21f1b30153e550dab79d7377bf29b16f4a8d6
mattaaronmagnaye <123015615+mattaaronmagnaye@users.noreply.github.com>
2023-06-23 22:30:23 -0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,65 +1,23 @@
 /*
-* Program: Blackjack - Project 7.4 (Assignment 2P)
 * Author: Matt Aaron Magnaye
-* Date: 06/17/2023
-* Instructor: Professor David Harden
-* Description: This program generates a random number between 1 to 10 each time player gets a card.
-*              It keeps the running total of the player's cards, and asks the player whethere or not it should deal another card.
-*              If the player hits 21 exactly, the program should print "Congratulations", if it exceeds 21, it prints "Bust".
-*              It also asks the player to play again or not.
-*
+* Professor: Dave Harden
+* Date: 06/23/23
+* Assignment 2P: Loops - Project 7.4
+* Description: It is a command line game that plays a simple version of blackjack.
+ *
 */
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
 
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
 using namespace std;
 
 int main() {
-    //srand(time(0));
+    srand(time(0));
 
-    char playAgain;
+    int first_card =
 
-    do {
-        // Generates two random cards
-        int card1 = rand() % 10 + 1;
-        int card2 = rand() % 10 + 1;
-        int totalCards = card1 + card2;
 
-        cout << "First cards: " << card1 << ", " << card2 << endl;
-        cout << "Total: " << totalCards << endl;
-
-        char addCard;
-        cout << "Do you want another card (y/n)? ";
-        cin >> addCard;
-
-        while (addCard == 'y') {
-            // Draws new card
-            int newCard = rand() % 10 + 1;
-            totalCards += newCard;
-            cout << "Card: " << newCard << endl;
-            cout << "Total: " << totalCards << endl;
-
-            // If number exceeds 21
-            if (totalCards > 21) {
-                cout << "Bust." << endl;
-                break;
-            }
-
-            // If number is 21
-            if (totalCards == 21) {
-                cout << "Congratulations!" << endl;
-                break;
-            }
-
-            cout << "Do you want another card (y/n)? ";
-            cin >> addCard;
-        }
-
-        // Asks user to play again
-        cout << "Would you like to play again (y/n)? ";
-        cin >> playAgain;
-    } while (playAgain == 'y');
-
+            cout << "First cards: " <<
     return 0;
 }
