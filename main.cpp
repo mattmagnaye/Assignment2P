e070911771aa50b226debf192ad38212d854b9b8
mattaaronmagnaye <123015615+mattaaronmagnaye@users.noreply.github.com>
2023-06-23 23:07:13 -0700

    added congrats-bust function

--- main.cpp
+++ main.cpp
@@ -14,15 +14,39 @@ using namespace std;
 
 int main() {
     srand(time(0));
+    
 
+    // Initialize first cards and output total
     int first_card = rand() % 10 + 1;
     int second_card = rand() % 10 + 1;
     int total_cards = (first_card + second_card);
 
-    cout << "First cards: " << ", " << second_card;
-    cout << "Total: " << total_cards;
+    // Output first 2 cards and total
+    cout << "First cards: " << first_card << ", " << second_card << endl;
+    cout << "Total: " << total_cards << endl;
 
-    cout << "Do you want another card"
+    // Asks the user if they want an another card
+    char addCard = 'y';
+    cout << "Do you want another card (y/n)? ";
+    cin >> addCard;
+    cout << "You entered: " << addCard << endl;
+
+    // Add card function
+    while (addCard == 'y') {
+        int another_card = rand() % 10 + 1;
+        total_cards = another_card + total_cards;
+        cout << "Total: " << total_cards << endl;
+
+    if (total_cards == 21) {
+        cout << "Congratulations!" << endl;
+        break;
+        }
+    else if ( total_cards > 21) {
+        cout << "Bust" << endl;
+        break;
+        }
+    }
+
 
 
     return 0;
