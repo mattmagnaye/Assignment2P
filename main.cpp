59f062355afc8727eba06e9c588f86de810bd635
mattaaronmagnaye <123015615+mattaaronmagnaye@users.noreply.github.com>
2023-06-23 23:18:17 -0700

    fully working program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 /*
-* Author: Matt Aaron Magnaye
-* Professor: Dave Harden
+* Author: Matt Aaron Magnaye - CIS 278
+* Instructor: Professor Dave Harden
 * Date: 06/23/23
 * Assignment 2P: Loops - Project 7.4
 * Description: It is a command line game that plays a simple version of blackjack.
- *
+*
 */
 
 #include <iostream>
@@ -14,9 +14,11 @@ using namespace std;
 
 int main() {
     srand(time(0));
+    char retry = 'y';
     
 
     // Initialize first cards and output total
+    while (retry == 'y'){
     int first_card = rand() % 10 + 1;
     int second_card = rand() % 10 + 1;
     int total_cards = (first_card + second_card);
@@ -34,20 +36,29 @@ int main() {
     // Add card function
     while (addCard == 'y') {
         int another_card = rand() % 10 + 1;
+        cout << "Card: " << another_card << endl;
         total_cards = another_card + total_cards;
         cout << "Total: " << total_cards << endl;
 
+    // If total cards is equal to 21
     if (total_cards == 21) {
         cout << "Congratulations!" << endl;
         break;
         }
-    else if ( total_cards > 21) {
-        cout << "Bust" << endl;
+
+    // If total cards is more than 21
+    if ( total_cards > 21) {
+        cout << "Bust." << endl;
         break;
         }
+        cout << "Do you want another card (y/n)? ";
+        cin >> addCard;
     }
 
-
+    // Asks the user if they want to retry the game
+    cout << "Would you like to play again (y/n)? ";
+    cin >> retry;
+    }
 
     return 0;
 }
